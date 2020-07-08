f2798db7984465d6ef1490a70d3cc8939898ab07
maciekto <ekstar007@gmail.com>
2020-07-08 16:50:32 +0200

    Add minigame when page is loading

--- src/components/PageLoad.vue
+++ src/components/PageLoad.vue
@@ -1,11 +1,36 @@
 <template>
     <div class="PageLoad">
+        <div class="PageLoad-Counter"> {{ clickCount }}</div>
         <div class="PageLoad-Title">Loading</div>
+        <div @click="RandomNumber" @mouseover="RandomNumber" class="PageLoad-Circle" :style="{top: randomNumberHeight + '%', left: randomNumberWidth + '%'}"></div>
     </div>
 </template>
 
 <script>
 export default {
   name: 'PageLoad',
+  data() {
+    return {
+      randomNumberWidth: null,
+      randomNumberHeight: null,
+      clickCount: 0,
+    };
+  },
+  mounted() {
+    this.RandomNumber();
+  },
+  methods: {
+    RandomNumber() {
+      const ValueForCheckWidth = Math.floor(Math.random() * 100) + 1;
+      const ValueForCheckHeight = Math.floor(Math.random() * 100) + 1;
+      if ((ValueForCheckWidth >= 60 || ValueForCheckWidth <= 40) && (ValueForCheckHeight >= 60 || ValueForCheckHeight <= 40) && (ValueForCheckWidth < 90) && (ValueForCheckHeight < 90)) {
+        this.clickCount += 1;
+        this.randomNumberWidth = ValueForCheckWidth;
+        this.randomNumberHeight = ValueForCheckHeight;
+      } else {
+        this.RandomNumber();
+      }
+    },
+  },
 };
 </script>
