fdafcd5671442e2d1f92e41ac9c50adc24bc873f
maciekto <ekstar007@gmail.com>
2020-07-08 18:19:35 +0200

    Improve naming variables and functions for better understanding code in main App.vue File

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
-    <PageLoad/>
-    <NavBar />
-    <NavBarModal />
-    <Video />
-    <Section1 />
-    <Section2 />
-    <Section3 />
-    <section4 />
-    <Footer />
+    <PageLoad/>     <!-- Loader which have minigame for user to play with until loading ends -->
+    <NavBar />      <!-- Title with link to CV and social media, Hamburger menu icon connected with modal -->
+    <NavBarModal /> <!-- Modal with links to page sections -->
+    <Video />       <!-- Video with custom video controls -->
+    <Section1 />    <!-- LATEST section - contains carousel width 3 latest projects -->
+    <Section2 />    <!-- ABOUT section - contains Title, Desription, photo and photo attribution of client -->
+    <Section3 />    <!-- TYPE OF WORK section - contains Main title and Title, Description, photo, photo attribution for each type of work -->
+    <section4 />    <!-- CONTACT section - contains Title and 2 photo with links to social media -->
+    <Footer />      <!-- FOOTER - links to social media, CV and All rights reserved -->
   </div>
 </template>
 
@@ -41,35 +41,35 @@ export default {
   },
   methods: {
     textAnimate() {
-      let prevScrollposN = window.pageYOffset;
-      // eslint-disable-next-line
-      window.addEventListener('scroll', onScroll);
-      function elementVisible(el) {
-        const top = el.offsetTop;
-        const height = el.offsetHeight;
-        const bottom = top + height;
+      function AddClassForAnimation(div) {
+        const topOfDiv = div.offsetTop;
+        const heightOfDiv = div.offsetHeight;
+        const bottomOfDiv = topOfDiv + heightOfDiv;
 
-        const IsOverBottom = top > (window.pageYOffset + window.innerHeight);
-        const IsBeforeTop = bottom < window.pageYOffset;
+        const IsOverBottom = topOfDiv > (window.pageYOffset + window.innerHeight);
+        const IsBeforeTop = bottomOfDiv < window.pageYOffset;
 
         if (!IsOverBottom && !IsBeforeTop) {
-          el.classList.add('show');
+          div.classList.add('show');
         } else {
-          el.classList.remove('show');
+          div.classList.remove('show');
         }
       }
-      function onScroll() {
+      let prevScrollposN = window.pageYOffset;
+      // eslint-disable-next-line
+      window.addEventListener('scroll', onScrollShowNavBar);
+      function onScrollShowNavBar() {
         // eslint-disable-next-line
-        for (const item of document.querySelectorAll('.Animate')) {
-          elementVisible(item);
+        for (const div of document.querySelectorAll('.Animate')) {
+          AddClassForAnimation(div);
         }
         // eslint-disable-next-line
-        for (const item of document.querySelectorAll('.Animate2')) {
-          elementVisible(item);
+        for (const div of document.querySelectorAll('.Animate2')) {
+          AddClassForAnimation(div);
         }
         // eslint-disable-next-line
-        for (const item of document.querySelectorAll('.Animate3')) {
-          elementVisible(item);
+        for (const div of document.querySelectorAll('.Animate3')) {
+          AddClassForAnimation(div);
         }
         // eslint-disable-next-line
         let currentScrollPosN = window.pageYOffset;
