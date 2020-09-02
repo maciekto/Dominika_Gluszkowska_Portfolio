874ede3e4a472330293cee0816fdf0e83ac88932
Maciej Tomaszewski <mt.maciejtomaszewski@gmail.com>
2020-09-02 19:19:29 +0200

    Finished app with content after issues with git

--- src/components/Section3.vue
+++ src/components/Section3.vue
@@ -7,7 +7,6 @@
             </p>
           </div>
           <!-- FIRST ARTICLE -->
-<<<<<<< HEAD
           <h3 class="Article3-TextTitle Article3-TextTitle1 Animate2"> UX/UI Design </h3>
           <p class="Article3-TextDescription Article3-TextDescription1 Animate2"> Homepage design for app, made in figma</p>
           <img src="./../assets/sztosApp.jpg" alt="article3-1" class="Article3-TextImg Animate2">
@@ -27,27 +26,6 @@
           <p class="Article3-TextDescription Article3-TextDescription4 Animate3"> Visualization of cups for a Cafe made in Photoshop </p>
           <img src="./../assets/cupCoffee.jpg" alt="article3-1" class="Article3-TextImg Animate3">
           <p class="Article3-TextImgAttribution Animate3"> Coffee Cups Visualisation </p>
-=======
-          <h3 class="Article3-TextTitle Article3-TextTitle1 Animate2"> Drawing</h3>
-          <p class="Article3-TextDescription Article3-TextDescription1 Animate2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in eros ut libero viverra fringilla. Maecenas ac maximus tellus, at vestibulum sem. Aenean egestas, enim non aliquam consectetur, nulla metus. </p>
-          <img src="./../beforeXD/article3-1.jpg" alt="article3-1" class="Article3-TextImg Animate2">
-          <p class="Article3-TextImgAttribution Animate2"> Drawing </p>
-          <!-- SECOND ARTICLE -->
-          <h3 class="Article3-TextTitle Article3-TextTitle2 Animate3"> LEAFLETS </h3>
-          <p class="Article3-TextDescription Article3-TextDescription2 Animate3"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in eros ut libero viverra fringilla. Maecenas ac maximus tellus, at vestibulum sem. Aenean egestas, enim non aliquam consectetur, nulla metus.</p>
-          <img src="./../beforeXD/article3-2.png" alt="article3-1" class="Article3-TextImg Article3-TextImg2 Animate3">
-          <p class="Article3-TextImgAttribution Animate3"> Leaflets </p>
-          <!-- THIRD ARTICLE -->
-          <h3 class="Article3-TextTitle Article3-TextTitle3 Animate2"> Painting</h3>
-          <p class="Article3-TextDescription Article3-TextDescription3 Animate2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in eros ut libero viverra fringilla. Maecenas ac maximus tellus, at vestibulum sem. Aenean egestas, enim non aliquam consectetur, nulla metus.</p>
-          <img src="./../beforeXD/article3-3.jpg" alt="article3-1" class="Article3-TextImg Animate2">
-          <p class="Article3-TextImgAttribution Animate2"> Paintings </p>
-          <!-- FOURTH ARTICLE -->
-          <h3 class="Article3-TextTitle Article3-TextTitle4 Animate3"> UX/UI DESIGN </h3>
-          <p class="Article3-TextDescription Article3-TextDescription4 Animate3"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in eros ut libero viverra fringilla. Maecenas ac maximus tellus, at vestibulum sem. Aenean egestas, enim non aliquam consectetur, nulla metus.</p>
-          <img src="./../beforeXD/article3-4.png" alt="article3-1" class="Article3-TextImg Animate3">
-          <p class="Article3-TextImgAttribution Animate3"> UX/UI Design </p>
->>>>>>> 34fb2d05910d08ab1a2b4d5b98265ed70067303a
         </article>
     </section>
 </template>
