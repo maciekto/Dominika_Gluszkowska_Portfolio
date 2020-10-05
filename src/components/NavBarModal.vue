66efe5a7b920cb77556c31a436d0f28cd993d09c
Maciej Tomaszewski <mt.maciejtomaszewski@gmail.com>
2020-10-05 12:50:08 +0200

    Comment navBar section

--- src/components/NavBarModal.vue
+++ src/components/NavBarModal.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="NavBar-Modal">
         <div class="NavBar-Modal-2"></div>
-        <a href="#latest" class="NavBar-ModalLink">LATEST PROJECTS</a>
+        <!-- <a href="#latest" class="NavBar-ModalLink">LATEST PROJECTS</a> -->
         <a href="#about" class="NavBar-ModalLink">ABOUT</a>
         <a href="#types" class="NavBar-ModalLink">TYPES OF WORKS</a>
         <a href="#contact" class="NavBar-ModalLink">CONTACT</a>
